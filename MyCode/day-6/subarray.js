c4457a04b6c55eeadf2f0eb3d1ebfe9da0aacb73
Prafull Varshney <prafullvarshney69@gmail.com>
2018-07-08 21:55:33 +0530

    subarray

--- MyCode/day-6/subarray.js
+++ MyCode/day-6/subarray.js
@@ -1,11 +1,11 @@
-var array=[-3,2,-2,3,6];
+var array=[-3,2,0,-2,3,6];
 var sum=0;
 var subArr=[];
 for(var i=0;i<array.length;i++)
 {
     sum+=array[i];
     subArr.push(array[i]);
-    if(sum==0 || array[i]==0)
+    if(sum==0)
     {
        console.log(subArr);
     }
