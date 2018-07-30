93826e9fa82d0321611729dc5999a729a8326387
Prafull Varshney <prafullvarshney69@gmail.com>
2018-07-30 23:36:51 +0530

    prime number or not !

---
+++ MyCode/day-4/Prime1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Prime1
+{
+public static void main(String ar[])
+{
+boolean prime=true;
+System.out.println("Enter number to check prime or not");
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+for(int i=2;i<num;i++)
+{
+if(num%i==0)
+{
+prime=false;
+}
+}
+if(prime==true)
+{
+System.out.println("Given number is a prime number");
+}
+else
+{
+System.out.println("Given number is not a prime number");
+}
+}
+}
